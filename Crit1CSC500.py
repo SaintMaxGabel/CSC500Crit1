d65a2cf62d552eab42afe9af1fae94875aef7744
Max G <max582@revature.net>
2024-02-26 01:15:09 -0500

    first commit

---
+++ Crit1CSC500.py
@@ -0,0 +1,11 @@
+num1AS = int(input("enter number one:"));
+num2AS = int(input("enter number two:"));
+
+print("number one - number two = " + str((num1AS - num2AS)) +
+      ", and number one + number two = " + str((num1AS+num2AS)));
+
+num3MD = int(input("enter number three:"));
+num4MD = int(input("enter number four:"));
+
+print("number three * number four = " + str((num3MD*num4MD)) +
+      ", and number three / number four = " + str((num3MD/num4MD)));
